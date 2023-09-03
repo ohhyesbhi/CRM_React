bf49510975783fd84a1d4c18d148d6e6c70236b1
unknown <kamatabhishek14@gmail.com>
2023-09-03 15:51:40 +0530

    added redux store and enabled the tools

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './Redux/Store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>  
 </BrowserRouter>
   ,
 )
